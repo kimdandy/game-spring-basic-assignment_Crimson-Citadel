774907fdc235f8a4024129658098e60c7f851ff3
kimdandy <d22psource@gmail.com>
2026-09-10 19:20:10 +0900

    Lv3;Make RESTFul API and fix error

--- src/main/java/com/gamebasic/game/controller/GameController.java
+++ src/main/java/com/gamebasic/game/controller/GameController.java
@@ -7,26 +7,24 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*; // 상위 라이브러리로 통합하여 사용
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/games") // 상위에서 라우팅을 했기 때문에 하위에서는 생략
 public class GameController {
     private final GameService gameService;
 
-    @GetMapping("/game")
+    @GetMapping
     public ResponseEntity<List<Object>> getGames() {
         // List<Object>는 임시 구현이며, Lv 7에서 제대로 고칩니다.
         // List.of()는 빈 목록을 돌려주는 임시 구현이며, Lv 7에서 제대로 고칩니다.
         return ResponseEntity.ok(List.of());
     }
 
-    @PostMapping("/games")
+    @PostMapping
     public ResponseEntity<GameDetailResponse> createGame(@Valid @RequestBody CreateRequest request) {
         GameDetailResponse created = gameService.createGame(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
